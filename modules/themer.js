61d21027987ced292b4580ebf4df957c9948eff7
Alana Gilston <abluescarab@users.noreply.github.com>
2025-02-15 17:04:28 -0800

    Return value from cycleData()

--- modules/themer.js
+++ modules/themer.js
@@ -1,6 +1,6 @@
 import { cycleData } from "./utils.js";
 export function cycleThemes(element, ...themes) {
-    cycleData(element, "theme", ...themes);
+    return cycleData(element, "theme", ...themes);
 }
 export function setTheme(element, theme = "") {
     return cycleThemes(element, theme);
--- src/modules/themer.ts
+++ src/modules/themer.ts
@@ -1,7 +1,7 @@
 import { cycleData } from "./utils.js";
 
 export function cycleThemes(element: HTMLElement, ...themes: string[]) {
-    cycleData(element, "theme", ...themes);
+    return cycleData(element, "theme", ...themes);
 }
 
 export function setTheme(element: HTMLElement, theme: string = "") {
