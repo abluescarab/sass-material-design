e4bd593af6c5a5674ba1e0b6d5ca856b1e044521
Alana Gilston <abluescarab@users.noreply.github.com>
2025-03-01 17:55:34 -0800

    Added TimedQueue

---
+++ dist/modules/timed-queue.d.ts
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * @file            modules/timed-queue.ts
+ * @description     Implements a queue which runs a callback function after a
+ *                  given delay.
+ ******************************************************************************/
+/**
+ * An item in a timed queue.
+ */
+export type TimedQueueItem<T> = {
+    callback: (item: TimedQueueItem<T>) => void;
+    data: T;
+    delay: number | undefined;
+};
+/**
+ * A timed queue which runs a callback function after a given delay.
+ */
+export default class TimedQueue<T> {
+    #private;
+    /**
+     * Gets the length of the queue.
+     * @returns length of queue
+     */
+    length: () => number;
+    /**
+     * Pushes an item or items onto the back of the queue.
+     * @param items items to push
+     */
+    push(...items: TimedQueueItem<T>[]): void;
+    /**
+     * Pops an item from the front or back of the queue.
+     * @returns popped item
+     */
+    pop(front?: boolean): TimedQueueItem<T> | undefined;
+    /**
+     * Clears all items from the queue.
+     * @param clearCallback function run on each cleared item
+     * @returns number of cleared items
+     */
+    clear(clearCallback?: (item: TimedQueueItem<T>) => void): number;
+    /**
+     * Removes an item from the queue.
+     * @param item item to remove
+     */
+    remove(item: TimedQueueItem<T>): void;
+}
---
+++ src/modules/timed-queue.ts
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * @file            modules/timed-queue.ts
+ * @description     Implements a queue which runs a callback function after a
+ *                  given delay.
+ ******************************************************************************/
+
+/**
+ * An item in a timed queue.
+ */
+export type TimedQueueItem<T> = {
+    callback: (item: TimedQueueItem<T>) => void;
+    data: T;
+    delay: number | undefined;
+};
+
+/**
+ * A timed queue which runs a callback function after a given delay.
+ */
+export default class TimedQueue<T> {
+    #queue: { item: TimedQueueItem<T>; timeout: number }[] = [];
+
+    /**
+     * Gets the length of the queue.
+     * @returns length of queue
+     */
+    length = () => this.#queue.length;
+
+    /**
+     * Pushes an item or items onto the back of the queue.
+     * @param items items to push
+     */
+    push(...items: TimedQueueItem<T>[]): void {
+        for (const item of items) {
+            this.#queue.push({
+                item: item,
+                timeout: setTimeout(() => {
+                    item.callback(item);
+                    this.remove(item);
+                }, item.delay ?? 0),
+            });
+        }
+    }
+
+    /**
+     * Pops an item from the front or back of the queue.
+     * @returns popped item
+     */
+    pop(front: boolean = false): TimedQueueItem<T> | undefined {
+        const shiftOrPop = front ? this.#queue.shift : this.#queue.pop;
+        const item = shiftOrPop();
+
+        clearTimeout(item?.timeout);
+        return item?.item;
+    }
+
+    /**
+     * Clears all items from the queue.
+     * @param clearCallback function run on each cleared item
+     * @returns number of cleared items
+     */
+    clear(clearCallback?: (item: TimedQueueItem<T>) => void): number {
+        const cleared = this.#queue.length;
+
+        for (const item of this.#queue) {
+            clearTimeout(item.timeout);
+
+            if (clearCallback) {
+                clearCallback(item.item);
+            }
+        }
+
+        this.#queue.splice(0, this.#queue.length);
+        return cleared;
+    }
+
+    /**
+     * Removes an item from the queue.
+     * @param item item to remove
+     */
+    remove(item: TimedQueueItem<T>): void {
+        const index = this.#queue.findIndex((q) => q.item == item);
+
+        if (index > -1) {
+            clearTimeout(this.#queue[index].timeout);
+            this.#queue.splice(index, 1);
+        }
+    }
+}
